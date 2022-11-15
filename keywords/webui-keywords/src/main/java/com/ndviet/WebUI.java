57a711f0e5555d25f99f45d2b2481b5906e1212c
Nguyen Duc Viet <nguyenducviet4496@gmail.com>
2022-11-15 16:58:30 +0700

    Update robot-fwk test to individual repo

--- keywords/webui-keywords/src/main/java/com/ndviet/WebUI.java
+++ keywords/webui-keywords/src/main/java/com/ndviet/WebUI.java
@@ -9,11 +9,11 @@ import java.util.Arrays;
 @RobotKeywords
 public class WebUI extends AnnotationLibrary {
     public WebUI() {
-        super(Arrays.asList("com/ndviet/automation/keywords/webui/WebUI.class"));
+        super(Arrays.asList("com/ndviet/WebUI.class"));
     }
 
     @RobotKeyword
     public void openBrowser() throws Exception {
-
+        BrowserManagement.getInstance().goToUrl("https://google.com");
     }
 }
