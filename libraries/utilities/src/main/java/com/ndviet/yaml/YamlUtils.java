0739c877ce8a97be1634329ee44b3e4905f1b6e8
Nguyen Duc Viet <nguyenducviet4496@gmail.com>
2022-12-23 11:09:26 +0700

    Updated YAML utilities

--- libraries/utilities/src/main/java/com/ndviet/yaml/YamlUtils.java
+++ libraries/utilities/src/main/java/com/ndviet/yaml/YamlUtils.java
@@ -2,8 +2,11 @@ package com.ndviet.yaml;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 
+import com.ndviet.file.FileUtils;
 import org.yaml.snakeyaml.Yaml;
 
 public class YamlUtils {
@@ -14,6 +17,16 @@ public class YamlUtils {
         return map;
     }
 
+    public static LinkedHashMap readAllYaml(String filePath) throws Exception {
+        Yaml yaml = new Yaml();
+        LinkedHashMap multiYaml = new LinkedHashMap<>();
+        InputStream input = new FileInputStream(filePath);
+        for (Object o : yaml.loadAll(input)) {
+            multiYaml.putAll((LinkedHashMap) o);
+        }
+        return multiYaml;
+    }
+
     public static LinkedHashMap readAllYaml(String filePath, String keyName, String valueName) throws Exception {
         Yaml yaml = new Yaml();
         LinkedHashMap<String, String> multiYaml = new LinkedHashMap<>();
@@ -21,6 +34,16 @@ public class YamlUtils {
         for (Object o : yaml.loadAll(input)) {
             LinkedHashMap map = (LinkedHashMap) o;
             multiYaml.put(map.get(keyName).toString(), map.get(valueName).toString());
+        }
+        return multiYaml;
+    }
+
+    public static LinkedHashMap readAllYamlInDirectory(String directory) throws Exception {
+        LinkedHashMap<String, String> multiYaml = new LinkedHashMap<>();
+        List<String> files = new ArrayList<>();
+        files = FileUtils.recursiveGetListFiles(directory, files, "y.*ml");
+        for(String filePath : files) {
+            multiYaml.putAll(readAllYaml(filePath));
         }
         return multiYaml;
     }
