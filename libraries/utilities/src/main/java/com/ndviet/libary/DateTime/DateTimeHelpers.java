531202552555a85ec1aeb1b88f2c405f01d91365
Nguyen Duc Viet <nguyenducviet4496@gmail.com>
2022-12-28 16:54:18 +0700

    Added DateTimeUtils

---
+++ libraries/utilities/src/main/java/com/ndviet/libary/DateTime/DateTimeHelpers.java
@@ -0,0 +1,30 @@
+package com.ndviet.libary.DateTime;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.IsoFields;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class DateTimeHelpers {
+    public static Map getCurrentQuarterYear() {
+        LocalDate localDate = LocalDate.now();
+        int quarter = localDate.get(IsoFields.QUARTER_OF_YEAR);
+        int year = localDate.getYear();
+        Map<String, String> returnDate = new LinkedHashMap<>();
+        returnDate.put("Year", year + "");
+        returnDate.put("Quarter", quarter + "");
+        return returnDate;
+    }
+
+    public static String getCurrentDateInFormat(String format, int increase) {
+        Calendar calendar = Calendar.getInstance();
+        Date today = new Date();
+        calendar.setTime(today);
+        calendar.add(Calendar.MONTH, increase);
+        Date date = calendar.getTime();
+        return new SimpleDateFormat(format).format(date);
+    }
+}
